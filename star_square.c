29d89db8b8d19cd1ecc5cf1de6c05f9f84f62c23
xtyzeq <hsgwfjalssjjsbajak@gmail.com>
2025-01-20 21:51:04 +0530

    star square completed

--- star_square.c
+++ star_square.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
+
 int main(){
     int n;
     printf("Enter the number of rows: ");
     scanf("%d", &n);
+
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            if(j==(n+1)/2 ||  i==(n+1)/2){
+            if(j==1 || j==n || i==1 || i==n){
                 printf("* ");
             }
             else{

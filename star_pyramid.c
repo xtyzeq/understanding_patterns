6c6794e5899ab7b65a2f53e367515f5a0f8e776b
xtyzeq <hsgwfjalssjjsbajak@gmail.com>
2025-01-21 01:09:10 +0530

    completed star_pyramid

--- star_pyramid.c
+++ star_pyramid.c
@@ -7,14 +7,12 @@ int main(){
     scanf("%d", &n);
 
     for(int i=1; i<=n; i++){
-        char alpha='A';
-        for(int j=1; j<=n; j++){
-            if(j<=n-i){
+        for(int j=1; j<=(2*n)-1; j++){
+            if(j<=n-i || j>=n+i){
                 printf("  ");
             }
             else{
-                printf("%2c", alpha);
-                alpha++;
+                printf("* ");
             }
         }
         printf("\n");

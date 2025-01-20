e9ae1ce988cfb7c4166a7fba38e1b9a89fd160ae
xtyzeq <hsgfjalssjjsbajak@gmail.com>
2025-01-20 15:32:25 +0530

    maybe a little confusing but believe me everything id fine

--- star_triangle_ulta.c
+++ star_triangle_ulta.c
@@ -5,7 +5,7 @@ int main(){
     printf("Enter the number of rows"); 
     scanf("%d", &n);
     for (int i=1; i<=n; i++){
-        for (int j=1; j<=n+1-i; j++){
+        for (int j=1; j<= n+1-i; j++){
             printf(" *");
         }
         printf("\n");

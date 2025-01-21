46a061f1027c95e1c20433660b870e41c5af1fcc
xtyzeq <hsgfjalssjjsbajak@gmail.com>
2025-01-21 17:36:34 +0530

    completed aplha_dinning_table and fixed number dining table problem of printing extra lines

--- alpha_dinning_table.c
+++ alpha_dinning_table.c
@@ -7,14 +7,14 @@ int main(){
     scanf("%d", &n);
 
     for(int i=1; i<=n; i++){
-        int num=1;
+        int num=65;
         for(int j=1; j<=(2*n)-1; j++){
            if(j<=n+1-i || j>=n-1+i){
-               printf("%2d",num);
-               
+               printf("%2c",num);
+            
            }
            else{
-               printf("  ");
+               printf("##");
            }
                num++;
 
